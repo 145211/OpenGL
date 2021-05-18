bc71ca83387d1fe81c98c13e82f6d23118cb559c
Pekureda <aragok3@o2.pl>
2021-05-18 16:32:23 +0200

    Project config, external dependencies, external libraries

--- CG-V_Project/main.cpp
+++ CG-V_Project/main.cpp
@@ -1,3 +1,14 @@
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_SWIZZLE
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 
 int main() {
