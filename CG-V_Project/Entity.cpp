64587bb3e49fbfacb93da84f2454d7fde602e96b
Pekureda <aragok3@o2.pl>
2021-06-03 14:26:19 +0200

    Entity.initBuffers() done

--- CG-V_Project/Entity.cpp
+++ CG-V_Project/Entity.cpp
@@ -1,5 +1,33 @@
 #include "Entity.h"
 
+void Entity::initBuffers()
+{
+	// Generate and bind VAO buffer
+	if (VAO) glDeleteVertexArrays(1, &this->VAO);
+	glGenVertexArrays(1, &this->VAO);
+	glBindVertexArray(this->VAO);
+
+	// Generate and bind VBO buffer
+	if (VBO) glDeleteBuffers(1, &this->VBO);
+	glGenBuffers(1, &this->VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+	// Send data to the buffer
+	glBufferData(GL_ARRAY_BUFFER, this->model.arraySize() * sizeof(Vertex), this->model.vertexData(), GL_STATIC_DRAW);
+
+	// Set pointers to attributes
+	// Position
+	glVertexAttribPointer(this->sp->a("vertex"), 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+	// Color
+	glVertexAttribPointer(this->sp->a("color"), 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+	// Normal
+	glVertexAttribPointer(this->sp->a("normal"), 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, normal));
+	// Texcoord
+	glVertexAttribPointer(this->sp->a("texCoord"), 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texCoord));
+
+	// Disable current attribute buffer
+	glBindVertexArray(0);
+}
+
 void Entity::setTexture(Texture* texture)
 {
 	this->texture = texture;
